49c4fcc7279f47bec618bd00f7ea7f1fadffec17
sal-rosa <danielvasconcelos148@gmail.com>
2022-06-13 20:41:36 -0300

    add remove host

--- src/adapters/redis-io-adapter.ts
+++ src/adapters/redis-io-adapter.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
@@ -7,7 +9,7 @@ export class RedisIoAdapter extends IoAdapter {
     private adapterConstructor: ReturnType<typeof createAdapter>;
 
     async connectToRedis(): Promise<void> {
-        const pubClient = createClient({ url: `redis://54.94.67.235:6379` });
+        const pubClient = createClient({ url: `redis://${process.env.HOST_REDIS}:6379` });
         const subClient = pubClient.duplicate();
 
         await Promise.all([pubClient.connect(), subClient.connect()]);
