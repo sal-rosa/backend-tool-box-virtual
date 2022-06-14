ae94dc0a2724afc4a2e095e3c716326f84163de6
sal-rosa <danielvasconcelos148@gmail.com>
2022-06-14 07:59:35 -0300

    remove path add namespace

--- src/convert-video/convert-video.gateway.ts
+++ src/convert-video/convert-video.gateway.ts
@@ -6,8 +6,7 @@ import { Socket, Server } from 'socket.io';
   cors: {
     origin: '*',
   },
-  path: '/api/tool-box-virtual/ws',
-  namespace: '/api/tool-box-virtual/ws/socket'
+  namespace: '/api/tool-box-virtual',
 })
 export class ConvertVideoGateWay {
   private logger: Logger = new Logger('ConvertVideoGateWay');
