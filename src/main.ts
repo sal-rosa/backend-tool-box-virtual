50fb1759f2285664593510049e759951b7d53e2b
sal-rosa <danielvasconcelos148@gmail.com>
2022-06-13 23:18:00 -0300

    remove cluster and adapter

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
-import { AppCluster } from './app.cluster';
-import { RedisIoAdapter } from './adapters/redis-io-adapter';
+//import { AppCluster } from './app.cluster';                         # For more process
+//import { RedisIoAdapter } from './adapters/redis-io-adapter';       # Adapt socket with reids
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const redisIoAdapter = new RedisIoAdapter(app);
-  await redisIoAdapter.connectToRedis();
+  //const redisIoAdapter = new RedisIoAdapter(app);
+  //await redisIoAdapter.connectToRedis();
 
-  app.useWebSocketAdapter(redisIoAdapter);
+  //app.useWebSocketAdapter(redisIoAdapter);
 
-  app.setGlobalPrefix('api/tool-box-virtual')
+  app.setGlobalPrefix('api/tool-box-virtual');
 
   app.enableCors({ origin: '*' });
 
   await app.listen(2000);
 }
 
-AppCluster.clusterize(bootstrap);
-//bootstrap();
+//AppCluster.clusterize(bootstrap); 
+bootstrap();
 
