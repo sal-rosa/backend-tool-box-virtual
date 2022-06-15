9bba975b0851fdd115275c47248fcbff84ecc29a
sal-rosa <danielvasconcelos148@gmail.com>
2022-06-14 22:06:29 -0300

    remove ws adapter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
-import { WsAdapter } from './adapters/ws-adapter';
 //import { AppCluster } from './app.cluster';                         # For more process
 //import { RedisIoAdapter } from './adapters/redis-io-adapter';       # Adapt socket with reids
 
@@ -10,7 +9,6 @@ async function bootstrap() {
 
   //const redisIoAdapter = new RedisIoAdapter(app);
   //await redisIoAdapter.connectToRedis();
-  app.useWebSocketAdapter(new WsAdapter(app));
 
   app.setGlobalPrefix('api/tool-box-virtual');
 
