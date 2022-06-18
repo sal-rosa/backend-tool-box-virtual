53ec124614a47900faad4ea82c3ddc29990b3d20
sal-rosa <danielvasconcelos148@gmail.com>
2022-06-18 20:35:28 -0300

    remove cpus limit

--- src/app.cluster.ts
+++ src/app.cluster.ts
@@ -1,16 +1,13 @@
 import * as notReallyCluster from 'cluster';
 const cluster = notReallyCluster as unknown as notReallyCluster.Cluster;
-import * as os from 'os';
 import { Injectable } from '@nestjs/common';
-
-const numCPUs = os.cpus().length;
 
 @Injectable()
 export class AppCluster {
     static clusterize(callback: Function): void {
         if(cluster.isPrimary){
             console.log(`Master server started on ${process.pid}`);
-            for (let i = 0; i < 1; i++) {
+            for (let i = 0; i < 2; i++) {
                 cluster.fork();
             }
             cluster.on('exit', (worker, code, signal) => {
